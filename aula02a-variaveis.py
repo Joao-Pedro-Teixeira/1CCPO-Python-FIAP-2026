a7634b750f36126bf45233213464cf5cbd8b4d19
Joao-Pedro-Teixeira <joaoppt1903@gmail.com>
2026-03-19 20:57:09 -0300

    Aula 2 variaveis e operadores

---
+++ aula02a-variaveis.py
@@ -0,0 +1,31 @@
+print ("Olá Mundo!")
+
+print (7+4)
+print ("7 + 4")
+print ("7" + "4")
+
+nome = "Joao Pedro"
+idade = 18
+peso = 63.5
+
+print (f"Meu nome é {nome}, tenho {idade} anos, e peso {peso} Kg.")
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+peso = float (input("Digite o seu peso atual: "))
+
+print (nome, idade, peso)
+print(idade+1)
+
+ano_Nascimento = 2008
+ano_Atual = 2026
+idade Atual = (ano_Atual - ano_Nascimento)
+print ("sua idade é {}")
+
+num1 = int(input("insira um numero: "))
+num2 = int(input("insira um numero"))
+num3 = num1 + num2
+print (f"A soma entre os")
+
+nome = input()
+
